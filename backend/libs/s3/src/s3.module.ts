ec8fab68807f3da50e25803fbbbd0979dab32b75
miqel-dll <michal.4d9eb834@gmail.com>
2026-04-02 05:42:29 +0200

    s3 handling service

--- backend/libs/s3/src/s3.module.ts
+++ backend/libs/s3/src/s3.module.ts
@@ -1,7 +1,9 @@
-import { Module } from "@nestjs/common";
+import { LoggerModule } from "@libs/logger";
 import { S3Service } from "./s3.service";
+import { Module } from "@nestjs/common";
 
 @Module({
+    imports: [LoggerModule.forFeature(S3Service)],
     providers: [S3Service],
     exports: [S3Service],
 })
--- backend/libs/s3/src/s3.service.ts
+++ backend/libs/s3/src/s3.service.ts
@@ -1,4 +1,66 @@
+import { InjectLogger } from "@libs/decorators";
 import { Injectable } from "@nestjs/common";
+import { Logger } from "@libs/logger";
+import { Client, RemoveOptions } from "minio";
+import Stream from "stream";
 
 @Injectable()
-export class S3Service {}
+export class S3Service {
+    private s3: Client;
+    constructor(@InjectLogger(S3Service) private readonly logger: Logger) {
+        const startTime = Date.now();
+        try {
+            this.s3 = new Client({
+                endPoint: process.env.MINIO_ENDPOINT,
+                port: +process.env.MINIO_PORT,
+                useSSL: false,
+                accessKey: process.env.MINIO_ROOT_USER,
+                secretKey: process.env.MINIO_ROOT_PASSWORD,
+            });
+        } catch (error) {
+            this.logger.error(`Failed to initialize S3 connection.`, { error, startTime });
+        }
+    }
+
+    public async putObject(bucketName: string, objectName: string, data: Buffer | Stream.Readable): Promise<unknown> {
+        try {
+            const createdObject = await this.s3.putObject(bucketName, objectName, data);
+            return {
+                path: `${process.env.MINIO_ENDPOINT}/${bucketName}/${objectName}`,
+                etag: createdObject.etag,
+            };
+        } catch (error) {
+            this.logger.error(`Failed to put object: ${objectName}, in bucket: ${bucketName}.`, { error });
+            return null;
+        }
+    }
+
+    public async getObject(bucketName: string, objectName: string): Promise<Buffer> {
+        try {
+            return await new Promise(async (resolve, reject) => {
+                const chunks: Buffer[] = [];
+                const stream = await this.s3.getObject(bucketName, objectName);
+                stream.on(`data`, (chunk) => chunks.push(chunk));
+                stream.on(`end`, () => resolve(Buffer.concat(chunks)));
+                stream.on(`error`, reject);
+            });
+        } catch (error) {
+            this.logger.error(`Failed to get buffer of: ${objectName} from bucket: ${bucketName}`);
+            return null;
+        }
+    }
+
+    public async deleteObject(bucketName: string, objectName: string, options?: RemoveOptions): Promise<boolean> {
+        try {
+            if (options) {
+                await this.s3.removeObject(bucketName, objectName, options);
+            } else {
+                await this.s3.removeObject(bucketName, objectName);
+            }
+            return true;
+        } catch (error) {
+            this.logger.error(`Failed to delete object: ${objectName}, from: ${bucketName}.`, { error });
+            return false;
+        }
+    }
+}
