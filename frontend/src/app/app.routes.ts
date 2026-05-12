dbacb4da4db95fe270f6042a7eba26269ba8955a
majqel-dll <michal.4d9eb834@gmail.com>
2026-05-12 19:57:48 +0200

    frontend application logical structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
+import { App } from './pages/app/app';
 
 export const routes: Routes = [
     { path: ``, pathMatch: `full`, component: App },
---
+++ frontend/src/app/pages/panel-page/panel-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-page',
+  imports: [],
+  templateUrl: './panel-page.html',
+  styleUrl: './panel-page.scss',
+})
+export class PanelPage {
+
+}
