08dbcbe779f00f2e2ace17274a50f39ce6f6c1cd
majqel-dll <michal.4d9eb834@gmail.com>
2026-05-13 06:24:48 +0200

    application visuals improvements

--- frontend/src/app/pages/home-page/homepage.ts
+++ frontend/src/app/pages/home-page/homepage.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
 import { SignInFormComponent } from '@functional/sign-in-form/sign-in-form';
+import { ViewportScroller } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
-  imports: [SignInFormComponent],
+  imports: [SignInFormComponent, RouterLink],
   templateUrl: './homepage.html',
   styleUrls: ['./homepage.scss', '../sign-in-page/sign-in-page.scss'],
 })
 export class Homepage {
+
+  protected readonly viewportScroller = inject(ViewportScroller);
+
+  public scrollToTop(): void {
+    this.viewportScroller.scrollToPosition([0, 0], { behavior: 'smooth' });
+  }
 
 }
--- frontend/src/app/pages/sign-in-page/sign-in-page.ts
+++ frontend/src/app/pages/sign-in-page/sign-in-page.ts
@@ -1,9 +1,10 @@
 import { SignInFormComponent } from '@functional/sign-in-form/sign-in-form';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in-page',
-  imports: [SignInFormComponent],
+  imports: [SignInFormComponent, RouterLink],
   templateUrl: './sign-in-page.html',
   styleUrls: ['./sign-in-page.scss'],
 })
